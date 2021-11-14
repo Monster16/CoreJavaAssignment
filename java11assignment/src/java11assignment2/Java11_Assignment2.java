daa3d5a9d6ee2e1d6554fd10e50b65d90aeb632a
anilysin <anilysin@LIN25007647.corp.capgemini.com>
2021-11-14 17:05:35 +0530

    Adding Java 11 assignment no 2

---
+++ java11assignment/src/java11assignment2/Java11_Assignment2.java
@@ -0,0 +1,8 @@
+package java11assignment2;
+
+public class Java11_Assignment2 {
+	
+	String path = "C:\\Users\\ANILYSIN\\git\\CoreJavaAssignment\\"
+			+ "java11assignment\\src\\java11assignment2";
+
+}
